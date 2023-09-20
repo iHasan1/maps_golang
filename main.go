038e40318027192beb9a0e582802b173b5ca8629
Imran Hasan <imran.h94@gmail.com>
2023-09-20 11:24:26 +0500

    practiced manipulating maps

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import "fmt"
 
 func main () {
 	// map declaration
-	colors := map[string]string{
-		"red": "#ff0000",
-		"green": "#4bf745",
-	}
+	// colors := map[string]string{
+	// 	"red": "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	// Others ways of kdeclaring maps
+	// var colors map[string]string // initializes with zero value
+	
+	//another way declaring maps
+	colors := make(map[string]string)
+
+	colors["white"] = " #ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
