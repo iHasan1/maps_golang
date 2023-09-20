912ddd1a003b77eebfab98af3d9089788f9dfc34
Imran Hasan <imran.h94@gmail.com>
2023-09-20 11:30:46 +0500

    practiced iterating over maps

--- main.go
+++ main.go
@@ -4,20 +4,17 @@ import "fmt"
 
 func main () {
 	// map declaration
-	// colors := map[string]string{
-	// 	"red": "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red": "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	// Others ways of kdeclaring maps
-	// var colors map[string]string // initializes with zero value
-	
-	//another way declaring maps
-	colors := make(map[string]string)
+	printMap(colors)
+}
 
-	colors["white"] = " #ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap (c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is =", hex );
+	}
 }
